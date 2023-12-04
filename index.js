cc3a52a09c9f37d7ea601b75e27bb1cbd01fd868
charbelfakhry <53810858+charbelfakhry@users.noreply.github.com>
2023-12-04 15:55:26 +0200

    partial views

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const ejs = require('ejs');
 const { fillDummyData, readUsers, getUsers } = require("./test");
 const { query } = require("./database/db");
 
+const schedule = require("node-schedule");
+
 const app = express();
 const PORT = process.env.PORT;
 
@@ -23,30 +25,72 @@ const { getTypicode, getTypicodeByUserId } = require("./services/fetchData");
 
 app.get('/', async(req, res)=>{
 
-    const formattedUsers = [];
-    const users = await query("select * from users");
-    for(const user of users)
-    {
-        let obj = {
-            user_id: user.user_id,
-            user_name: user.user_name,
-            user_dob: moment(user.user_dob).format("YYY-MM-DD"),
-        }
-        formattedUsers.push(obj);
-    }
+    const clients = await query(`SELECT 
+    client.*, COUNT(client_order.client_id) AS order_count
+FROM
+    client
+        LEFT OUTER JOIN
+    client_order ON client.client_id = client_order.client_id
+GROUP BY client_id;`);
+
     const data = {
         message: "welcome from ejs todays is ",
         username: "user001",
         content: "this is a very descriptive content",
-        users: formattedUsers,
+        users: clients,
     }
 
     res.render('index', data);
 
 });
 
+app.get("/testingroute", async(req, res)=>{
+    let sql = "select * from users";
+    let result = await query(sql);
+    res.status(200).json({
+        message: "test",
+        users: result,
+    })
+});
+
+app.post("/testingPost", async(req, res)=>{
+    try{
+        const {userName} = req.body;
+        let sql = `SELECT * FROM users WHERE user_username = ?`;
+        console.log(sql);
+        let result = await query(sql, [userName]);
+        res.status(200).json({
+            user: result
+        });
+    }catch(e){
+        res.status(500).json({
+            error: "Error executing query",
+        })
+    }
+
+})
+
 
 app.use('/api/users', userRoute);
+
+
+const startCronJob = () => {
+    const job = schedule.scheduleJob('* */10 * * *', () => {
+        console.log("This task is running at : ", new Date());
+    })
+}
+
+const startSpecificCronJob = (date) => {
+    const job = schedule.scheduleJob(date, () => {
+        console.log("This task is running at : ", new Date());
+    })
+}
+
+//startCronJob();
+const specificTime = new Date(Date.now() + 20 * 1000);
+startSpecificCronJob(specificTime);
+
+
 
 app.listen(PORT);
 
